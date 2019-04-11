ac5b2998e354750903d93d4fbd68781668f718a4
pfoubet <pfoubet@seriane.fr>
2019-04-11 14:01:00 +0200

    Version 1.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # RPI3bpXeno3
 
-Image Debian + Xeno 3.0.7
+Image Raspbian + Xeno 3.0.7
 Cours Temps-Réel de P. Foubet
 
 
--- recolle.sh
+++ recolle.sh
@@ -3,6 +3,7 @@
 SOURCE=RPI3bpXeno3.img.xz
 PRE=RPI3bpX
 
+
 [ -e $SOURCE ] && rm -f $SOURCE
 touch $SOURCE
 for A in a b c d e f g h i j k l m n o p q r s t u v w x y z ; do
